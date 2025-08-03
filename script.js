a541229611104f7e28a71117eb91f85c0365537d
Дмитрий А. Мищуков <d.michukov@jusanmobile.kz>
2025-08-03 16:00:15 +0500

    Поменял admin-login на admin_login

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
   const password = document.getElementById("password").value;
 
   try {
-    const response = await fetch("https://jusik-servak-bd.onrender.com/admin-login", {
+    const response = await fetch("https://jusik-servak-bd.onrender.com/admin_login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
